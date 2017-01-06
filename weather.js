800d23b014cdb601226abb6e123bb606ba829b48
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-01-06 14:02:45 +0100

    Added geolocation in js

--- weather.js
+++ weather.js
@@ -1,1 +1,19 @@
 var key = config.key;
+
+var x = document.getElementById("location-text");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+
+$(document).ready(function() {
+  getLocation();
+});
